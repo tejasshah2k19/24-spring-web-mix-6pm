4d783b06e203f002a8a13f188380af8a73a8853a
Tejas Shah <tejasshah2k19@gmail.com>
2024-06-14 18:59:16 +0530

    insert user

---
+++ src/main/java/com/controller/EcomSessionController.java
@@ -0,0 +1,36 @@
+package com.controller;
+
+import org.apache.catalina.startup.UserDatabase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bean.EUserBean;
+import com.dao.EUserDao;
+
+ 
+@Controller
+public class EcomSessionController {
+
+	@Autowired
+	EUserDao userDao;
+	
+	
+	@GetMapping("/esignup")
+	public String signup() {
+		return "EcomSignup"; 
+	}
+
+	@PostMapping("/esignup")
+	public String signupPost(EUserBean userBean) {
+		//jsp form -> controller -> 
+		
+		//validation 
+		
+		//insert  	
+		userDao.insertUser(userBean);
+		
+		return "EcomLogin";
+	}
+}
---
+++ src/main/java/com/dao/EUserDao.java
@@ -0,0 +1,27 @@
+package com.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.bean.EUserBean;
+
+//singleton -> new -> @Autowired 
+@Repository
+public class EUserDao {
+
+	@Autowired // instance eject
+	JdbcTemplate stmt; // update() query()
+
+	public void insertUser(EUserBean userBean) {
+		// query ->
+		stmt.update("insert into users(firstName,email,password) values (?,?,?) ", userBean.getFirstName(),
+				userBean.getEmail(), userBean.getPassword());
+
+	}
+
+}
+
+//controller -> USerBEan user  -> dao->insertUser(user) 
+
+//dao -> insertUser()
